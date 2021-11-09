9d3f7aa1ca31870d2739c29045de6295100872cd
jaak322 <79160292+jaak322@users.noreply.github.com>
2021-11-09 11:19:17 +0100

    Create face_detection.py
    face-detection

---
+++ face_detection.py
@@ -0,0 +1,72 @@
+import face_recognition
+import imutils
+import pickle
+import time
+from imutils.video import VideoStream
+from imutils.video import FPS
+import cv2
+from pydub import AudioSegment
+from pydub.playback import play
+from firebase import firebase
+import serial
+import time
+import re
+
+FBConn = firebase.FirebaseApplication('https://smart-house-ae2d9-default-rtdb.europe-west1.firebasedatabase.app/', None)
+
+song = AudioSegment.from_mp3("soundclip.mp3")
+
+currentname = "unknown"
+autolamp=1
+
+encodingsP = "encodings.pickle"
+
+print("Loading face detection & extraction of characteristics")
+data = pickle.loads(open(encodingsP, "rb").read())
+
+vs = VideoStream(src=0,framerate=10).start()
+time.sleep(2.0)
+
+fps = FPS().start()
+
+while True:
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	boxes = face_recognition.face_locations(frame)
+	encodings = face_recognition.face_encodings(frame, boxes)
+	names = []
+
+	for encoding in encodings:
+
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+
+		if True in matches:
+            
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+			
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			name = max(counts, key=counts.get)
+
+			if currentname != name:
+				currentname = name
+				if name == "Alex":
+				     print("welcome home Alex ")
+				     print("relaxing music is playing")
+				     lamp = FBConn.put("/Lamps","lamp1",autolamp)
+				     play(song)
+				     
+				     
+
+		names.append(name)
+		
+	fps.update()
+
+fps.stop()
+
+cv2.destroyAllWindows()
+vs.stop()
